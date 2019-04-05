cd47709991310b344083e1c0170476992c01b731
jftorresp <jf.torresp@uniandes.edu.co>
2019-04-04 19:13:57 -0500

    algoritmos

--- src/srcProyecto/Cliente.java
+++ src/srcProyecto/Cliente.java
@@ -9,49 +9,72 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import org.bouncycastle.cert.X509v2CRLBuilder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.x509.X509V2CRLGenerator;
 
 public class Cliente {
-	
+
 	static Socket s;
+
+	public final static String ALGORTIMO1 = "Blowfish";
+
+	public final static String ALGORITMO2 = "RSA";
+
+	public final static String ALGORTIMO3 = "HMACSHA256";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		try {
-			
-			
+
+
 			s = new Socket("localhost", 6790);
-			
+
 			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-			
+
 			OutputStream os = s.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
- 
-            String mensaje = "HOLA";
- 
-            String sendMessage = mensaje + "\n";
-            bw.write(sendMessage);
-            bw.flush();
-            System.out.println("Message sent to the server : "+sendMessage);
- 
-            //Get the return message from the server
-            InputStream is = s.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String message = br.readLine();
-            System.out.println("Message received from the server : " +message);
-			
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+
+			String mensaje = "HOLA";
+
+			String sendMessage = mensaje + "\n";
+			bw.write(sendMessage);
+			bw.flush();
+			System.out.println("Message sent to the server : "+sendMessage);
+
+			//Get the return message from the server
+			InputStream is = s.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String message = br.readLine();
+			System.out.println("Message received from the server : " +message);
+
 			if(message.equals("OK"))
 			{
-	            mensaje = "ALGORITMOS";
-	            sendMessage = mensaje + "\n";
-	            bw.write(sendMessage);
-	            bw.flush();
-	            System.out.println("Message sent to the server : "+sendMessage);
+				mensaje = "ALGORITMOS";	
+
+				sendMessage = mensaje + ":" + ALGORTIMO1 + ":" + ALGORITMO2 + ":" + ALGORTIMO3 + "\n";
+
+				bw.write(sendMessage);
+				bw.flush();
+				System.out.println("Message sent to the server : "+sendMessage);   
+				message = br.readLine();
+				System.out.println("Message received from the server : " +message);
+
+				if(message.equals("OK"))
+				{
+					
+				}
 			}
+
+
+
 			s.close();
-			
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -59,7 +82,7 @@ public class Cliente {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 	}
 
 }
